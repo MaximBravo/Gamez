038fd7f8bfafcabecc09dd94d93e0a99fd97ff66
Maxim Bravo <maxim@alexbravo.com>
2016-12-15 14:39:03 -0800

    Show X and Y coordinates of a touch

--- app/src/main/java/com/example/maximbravo/gamez/MainActivity.java
+++ app/src/main/java/com/example/maximbravo/gamez/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.maximbravo.gamez;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +31,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        View main = findViewById(R.id.content_main);
+        TextView t = (TextView) findViewById(R.id.test_textview);
+        if(event.getAction() == android.view.MotionEvent.ACTION_UP){
+            t.setText("Recording stoped.");
+        } else {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            t.setText("Recording: x:" + x + ", y:" + y);
+            int mainX = (int) main.getX();
+            if (x <= mainX) {
+                main.setBackgroundColor(Color.RED);
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/maximbravo/gamez/RecognizeTouchEvent.java
@@ -0,0 +1,8 @@
+package com.example.maximbravo.gamez;
+
+/**
+ * Created by Maxim Bravo on 12/15/2016.
+ */
+
+public class RecognizeTouchEvent {
+}
